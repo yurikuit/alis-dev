0ef02b75314a007908a37b579d8357d7f08fcc01
yurikuit <yuri.kuit+github@gmail.com>
2022-06-09 13:09:38 +0200

    config changes

--- alis.sh
+++ alis.sh
@@ -1605,7 +1605,7 @@ function desktop_environment_gnome() {
 }
 
 function desktop_environment_kde() {
-    pacman_install "plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kde-graphics-meta kde-multimedia-meta kde-network-meta"
+    pacman_install "plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kde-graphics-meta kde-multimedia-meta kde-network-meta packagekit-qt5"
 }
 
 function desktop_environment_xfce() {
